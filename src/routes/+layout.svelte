57b27d845489a0be4c3d22d685a798f9cf297651
Jax Bulbrook <jaxhacks543@gmail.com>
2024-04-08 17:15:59 -0400

    made tailwind work

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import '../app.css';
+</script>
+
+<html lang="en">
+	<slot />
+</html>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,5 @@
+<div class="flex items-center">
+    <p>a</p>
+    <p>a</p>
+    <p>a</p>
+</div>
