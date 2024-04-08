51ac5aea55097e30ce369c6660f28d5d512571af
Jax Bulbrook <jaxhacks543@gmail.com>
2024-04-08 17:01:28 -0400

    init

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    darkMode: 'selector',
+    theme: {
+      extend: {},
+    },
+    plugins: [require('daisyui')],
+    darkMode: 'media'
+  }
