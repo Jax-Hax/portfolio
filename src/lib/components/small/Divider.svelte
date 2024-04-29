e923ff87ca4dec80d412c5147109fe2d16b03200
Jax Bulbrook <jaxhacks543@gmail.com>
2024-04-29 17:13:25 -0400

    added a divider

---
+++ src/lib/components/small/Divider.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let src: string;
+</script>
+<div class="flex justify-center items-center">
+    <div class="w-[50px] border-b-[1px] border-dashed"></div>
+    <img {src} alt="divider" class="w-[30px] mx-3">
+    <div class="w-[50px] border-b-[1px] border-dashed"></div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
 	import Header from "./Header.svelte";
 	import Hero from "./Hero.svelte";
+	import WhatWeDo from "./WhatWeDo.svelte";
 </script>
 <Header currentPage="Home"/>
 <div class="max-w-[1100px] mx-auto">
     <Hero />
+	<WhatWeDo />
 </div>
