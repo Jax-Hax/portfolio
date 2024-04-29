d62bf666ce59761dc6af29df7bf092ebd5bc0d56
Jax Bulbrook <jaxhacks543@gmail.com>
2024-04-29 17:16:52 -0400

    styling

--- src/routes/WhatWeDo.svelte
+++ src/routes/WhatWeDo.svelte
@@ -1,21 +1,38 @@
 <script>
-	import Divider from "$lib/components/small/Divider.svelte";
-
+	import Divider from '$lib/components/small/Divider.svelte';
 </script>
-<h1 class="text-center font-bold text-4xl uppercase mt-10"><span class="text-primary">What</span> we do</h1>
-<Divider src="favicon.png"/>
-<p>We specialize in small business web design and development for clients anywhere in the US. Every line of code is written by hand to ensure the best performance, which helps bring in more customers to your site and bring more revenue to your business. LEARN MORE</p>
+
+<h1 class="text-center font-bold text-4xl uppercase mt-10">
+	<span class="text-primary">What</span> we do
+</h1>
+<div class="my-5">
+	<Divider src="favicon.png" />
+</div>
+<p class="max-w-xl mx-auto text-center mb-10">
+	We specialize in small business web design and development for clients anywhere in the US. Every
+	line of code is written by hand to ensure the best performance, which helps bring in more
+	customers to your site and bring more revenue to your business. LEARN MORE
+</p>
 <div class="flex">
 	<div>
 		<h1>Mobile-First Design</h1>
-		<p>We start by building your site for mobile viewing, which makes it rank much higher on Google Search.</p>
+		<p>
+			We start by building your site for mobile viewing, which makes it rank much higher on Google
+			Search.
+		</p>
 	</div>
 	<div>
 		<h1>Fully Responsive</h1>
-		<p>Your website will fit all mobile screens sizes, tablets, and desktop sizes so new clients can access your site from anywhere.</p>
+		<p>
+			Your website will fit all mobile screens sizes, tablets, and desktop sizes so new clients can
+			access your site from anywhere.
+		</p>
 	</div>
 	<div>
 		<h1>Optimization</h1>
-		<p>60% of all internet traffic is on mobile devices, so we optimize your mobile to perform their best in search engines.</p>
+		<p>
+			60% of all internet traffic is on mobile devices, so we optimize your mobile to perform their
+			best in search engines.
+		</p>
 	</div>
 </div>
