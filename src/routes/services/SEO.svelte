eac42c3aa5d5b3c481b3a2d2aec0cfdf13e27fff
Jax-Hax <jaxhacks543@gmail.com>
2024-05-26 16:36:17 -0400

    fixed image

--- src/routes/services/SEO.svelte
+++ src/routes/services/SEO.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex gap-7 xl:-ml-20 items-center justify-center flex-col lg:flex-row">
 	<div class="lg:w-1/2">
-		<h1 class="text-5xl font-bold">Search Engine Optimization For 2024</h1>
+		<h1 class="text-4xl leading-[1.1] font-bold">Search Engine Optimization For 2024</h1>
 		<p class="text-lg mt-7">
 			If someone tells you they can get you to the front page of Google in 3 months - <span class="text-primary font-bold">RUN!</span> Unless
 			your website was featured by The New York Times it could take years to rank in the top of the
