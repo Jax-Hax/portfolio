313841064cb63609c3417efebcfa2d98d7b13d36
Jax-Hax <jaxhacks543@gmail.com>
2024-05-26 11:10:33 -0400

    added proper spacing

--- src/routes/BottomBar.svelte
+++ src/routes/BottomBar.svelte
@@ -8,25 +8,25 @@
 			<span class="sr-only">Bulbrook Web Designs</span>
 			<Picture mobileSrc="favicon.png" width="100" alt="logo" />
 		</a>
-		<div><a href="mailto:jax@bulbrookwebdesigns.com">jax@bulbrookwebdesigns.com</a></div>
+		<div class="mb-3"><a href="mailto:jax@bulbrookwebdesigns.com">jax@bulbrookwebdesigns.com</a></div>
 		<div><a href="tel:804-821-8230">(804) 821-8230</a></div>
 	</div>
     <div>
 		<h1 class="font-bold text-3xl">Navigation</h1>
         <div class="divider divider-primary"></div> 
-		<div><a class="link link-hover transition" href="/">Home</a></div>
-		<div><a class="link link-hover transition" href="/">About</a></div>
-		<div><a class="link link-hover transition" href="/">Services</a></div>
-		<div><a class="link link-hover transition" href="/">Portfolio</a></div>
-		<div><a class="link link-hover transition" href="/">Contact</a></div>
+		<div class="my-1"><a class="link link-hover transition" href="/">Home</a></div>
+		<div class="my-1"><a class="link link-hover transition" href="/">About</a></div>
+		<div class="my-1"><a class="link link-hover transition" href="/">Services</a></div>
+		<div class="my-1"><a class="link link-hover transition" href="/">Portfolio</a></div>
+		<div class="my-1"><a class="link link-hover transition" href="/">Contact</a></div>
 	</div>
     <div>
 		<h1 class="font-bold text-3xl">Services</h1>
         <div class="divider divider-primary"></div> 
-		<div><a class="link link-hover transition" href="/">Web Design</a></div>
-		<div><a class="link link-hover transition" href="/">Website Maintenance</a></div>
-		<div><a class="link link-hover transition" href="/">SEO Services</a></div>
-		<div><a class="link link-hover transition" href="/">Hosting</a></div>
-		<div><a class="link link-hover transition" href="/">Lifetime Updates</a></div>
+		<div class="my-1"><a class="link link-hover transition" href="/">Web Design</a></div>
+		<div class="my-1"><a class="link link-hover transition" href="/">Website Maintenance</a></div>
+		<div class="my-1"><a class="link link-hover transition" href="/">SEO Services</a></div>
+		<div class="my-1"><a class="link link-hover transition" href="/">Hosting</a></div>
+		<div class="my-1"><a class="link link-hover transition" href="/">Lifetime Updates</a></div>
 	</div>
 </div>
