dcbb2c9788099b4ebcb61f6647bf5bf29bdb719e
Jax-Hax <jaxhacks543@gmail.com>
2024-05-26 13:45:27 -0400

    added better bg gradient

--- src/routes/BottomBar.svelte
+++ src/routes/BottomBar.svelte
@@ -2,7 +2,7 @@
 	import Picture from '$lib/components/small/Picture.svelte';
 </script>
 
-<div style="background: linear-gradient(145deg,#001099 0,#3ad9ff 96%)" class="mt-40">
+<div style="" class="mt-40 bg-gradient-to-r from-cyan-500 to-blue-500 dark:from-blue-700 dark:to-blue-900">
 	<div class="w-full">
 		<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
 			<path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="fill-base-100"></path>
