da09de5bcbbe44959b128c3c939caea39de2a982
Jax-Hax <jaxhacks543@gmail.com>
2024-05-26 11:15:28 -0400

    added height and width to dividers

--- src/lib/components/small/Divider.svelte
+++ src/lib/components/small/Divider.svelte
@@ -3,6 +3,6 @@
 </script>
 <div class="flex justify-center items-center">
     <div class="w-[50px] border-b-[1px] border-dashed"></div>
-    <img {src} alt="divider" class="w-[30px] mx-3" loading="lazy" decoding="async">
+    <img {src} alt="divider" class="w-[30px] mx-3" loading="lazy" decoding="async" height="30" width="30">
     <div class="w-[50px] border-b-[1px] border-dashed"></div>
 </div>
