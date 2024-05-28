9112d372031863c2a709b32c7fc36a10766e7a67
Jax Bulbrook <jaxhacks543@gmail.com>
2024-05-28 09:38:48 -0400

    changing more text

--- src/routes/WhoWeAre.svelte
+++ src/routes/WhoWeAre.svelte
@@ -4,17 +4,20 @@
 </script>
 
 <h1 class="text-4xl font-bold uppercase text-center mb-7">
-	<span class="text-primary">Who</span> we are
+	<span class="text-primary">Who</span> I am
 </h1>
 <Divider src="person.png" />
 <p class="mt-7 max-w-2xl text-center mx-auto">
-	My name is Jax, I am a high school student and web developer. I started this business to help
-	learn sales skills and be able to afford college. I learned how to code when I was 9 since I
-	wanted to make games for my friends. Now after having coded for 7 years I am confident enough to
-	charge for my work and have my method down to a science. <a class="font-semibold text-primary" href="/about">ABOUT US</a>
+	My name is Jax, I am a high school student and web developer. I started this business because I
+	love coding want to be able to afford college for a Computer Science Degree. I learned how to code
+	when I was 9 after being inspired by games such as Minecraft. Now after having coded for 7 years I
+	am confident enough to charge for my work and have my method down to a science. <a
+		class="font-semibold text-primary"
+		href="/about">ABOUT ME</a
+	>
 </p>
 <div class="flex flex-col items-center mt-20">
-<Picture mobileSrc="coding.png" alt="optimization" width="290" height="290" />
-<h2 class="font-bold mt-10 mb-2">Jax Bulbrook</h2>
-<p class="uppercase">Owner/Developer</p>
+	<Picture mobileSrc="coding.png" alt="optimization" width="290" height="290" />
+	<h2 class="font-bold mt-10 mb-2">Jax Bulbrook</h2>
+	<p class="uppercase">Owner/Developer</p>
 </div>
