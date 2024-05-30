ed24f106f8931e5e5542aa56836cdbda1b795b54
Jax-Hax <jaxhacks543@gmail.com>
2024-05-30 09:47:58 -0400

    changing analytics

--- src/lib/components/small/Analytics.svelte
+++ src/lib/components/small/Analytics.svelte
@@ -1,31 +1,9 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-
-	$: {
-		if (typeof gtag !== 'undefined') {
-			gtag('config', 'G-03C7TFW38R', {
-				page_title: document.title,
-				page_path: $page.url.pathname
-			});
-		}
-	}
 	export let title: string;
 	export let description: string;
 </script>
 
 <svelte:head>
-	<script defer src="https://www.googletagmanager.com/gtag/js?id=G-03C7TFW38R">
-	</script>
-	<script defer>
-		window.dataLayer = window.dataLayer || [];
-
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-
-		gtag('js', new Date());
-		gtag('config', 'G-03C7TFW38R');
-	</script>
 	<title>{title}</title>
 	<meta name="description" content={description} />
 </svelte:head>
